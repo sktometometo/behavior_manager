cbf1c23334ce779216d842838a58172c7f5739b3
Koki Shinjo <sktometometo@gmail.com>
2022-03-25 14:35:43 +0900

    [behavior_manager_server] update

---
+++ behavior_manager_server/test/test_behavior_manager.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+import roslib
+import unittest
+import actionlib
+
+from behavior_manager_msgs.msg import ExecuteBehaviorsAction
+from behavior_manager_msgs.msg import ExecuteBehaviorsGoal
+
+PKG = 'behavior_manager_server'
+roslib.load_manifest(PKG)
+
+
+class TestBehaviorManager(unittest.TestCase):
+
+    def test_behavior_manager_client(self):
+        target_node_id = rospy.get_param('~target_node_id')
+        client = actionlib.SimpleActionClient('~execute_behaviors', ExecuteBehaviorsAction)
+        client.wait_for_server()
+        goal = ExecuteBehaviorsGoal()
+        goal.target_node_id = target_node_id
+        client.send_goal(goal)
+        client.wait_for_result()
+        result = client.get_result()
+        self.assertTrue(result.success)
+
+
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun(PKG, 'test_behavior_manager', TestBehaviorManager)
